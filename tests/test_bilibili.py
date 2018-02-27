3a46ed522d3a846ddb81babfd7d582a23593efcf
comwrg <xcomwrg@gmail.com>
2018-02-27 20:36:31 +0800

    add test upload

--- tests/test_bilibili.py
+++ tests/test_bilibili.py
@@ -1,18 +1,30 @@
+import os
+import re
+import random
 import unittest
 from bilibili import Bilibili
 
 USERNAME = ''
 PASSWORD = ''
 
+b = Bilibili()
 
 class TestBilibili(unittest.TestCase):
     def test_login(self):
-        self.b = Bilibili()
-        r = self.b.login(USERNAME, PASSWORD)
+        r = b.login(USERNAME, PASSWORD)
         self.assertTrue(r)
 
     def test_upload(self):
-        pass
+        VIDEOS_PATTERN = '.+\.[mp4|flv|avi|wmv|mov|webm|mpeg4|ts|mpg|rm|rmvb|mkv]'
+        videos = [f for f in os.listdir('bin') if re.match(VIDEOS_PATTERN, f, re.I)]
+        for f in videos:
+            filepath = os.path.abspath('./bin/' + f)
+            title = str(random.randint(1, 10000)) + os.path.basename(filepath)
+            tid = 65
+            tag = ['test', '测试']
+            desc = 'desc, 描述'
+            b.upload(filepath, title, tid, tag, desc)
+
 
 
 if __name__ == '__main__':
